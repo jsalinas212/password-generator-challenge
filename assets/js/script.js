44d4830049f825b765b5d6924065fc65182180f1
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-05 23:51:31 -0600

    finished password generation logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 // Assignment code here
+var formEl = document.querySelector("#selectForm");
 
-
-// Get password length
-var passwordLength = 24;
 // Character types
 var specialChars = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",";"];
 var lowerAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -13,9 +11,32 @@ var getRandom = function(max) {
   return Math.floor(Math.random() * max);
 }
 
-var generatePassword = function() {
-  // Array of available types
-  var charTypes = [specialChars, lowerAlpha, numeric, upperAlpha];
+var generatePassword = function(passObj) {
+  // Get password options
+  var passwordLength = passObj.passLen;
+  var charTypes = [];
+
+  if (passObj.charNum != null) {
+    charTypes.push(numeric);
+  }
+
+  if (passObj.charUC != null) {
+    charTypes.push(upperAlpha);
+  }
+
+  if (passObj.charLC != null) {
+    charTypes.push(lowerAlpha);
+  }
+
+  if (passObj.charSpec != null) {
+    charTypes.push(specialChars);
+  }
+
+  if (passObj.charNum === null && passObj.charUC === null && passObj.charLC === null && passObj.charSpec === null) {
+    alert("You need to make at least one character type selection.");
+    return "";
+  }
+
   var charArrSize = charTypes.length;
 
   var passGen = function() {
@@ -25,7 +46,7 @@ var generatePassword = function() {
 
     // Take types and build password by length
     for (var i = 0; i < passwordLength; i++) {
-      var typeSelector = charTypes[getRandom(4)];
+      var typeSelector = charTypes[getRandom(charArrSize)];
       var typeArrSize = typeSelector.length;
       var charSelector = typeSelector[getRandom(typeArrSize)];
       passBuild.push(charSelector);
@@ -48,13 +69,41 @@ var generatePassword = function() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(passObj) {
+  var password = generatePassword(passObj);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Password event handler
+var passHandler = function(event) {
+  event.preventDefault();
+
+  // Collect form input
+  var charNum = document.querySelector("#charNum:checked");
+  var charUC = document.querySelector("#charUC:checked");
+  var charLC = document.querySelector("#charLC:checked");
+  var charSpec = document.querySelector("#charSpec:checked");
+  var passLen = document.querySelector("input[name='passLen']").value;
+
+  if (!passLen || passLen < 8 || passLen > 128) {
+    alert("Please make sure password length is between 8 and 128.");
+  }
+
+  // Build password object from form data
+  var passObj = {
+    charNum: charNum,
+    charUC: charUC,
+    charLC: charLC,
+    charSpec: charSpec,
+    passLen: passLen
+  }
+
+  // Pass object data to writePassword function
+  writePassword(passObj);
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+formEl.addEventListener("submit", passHandler);
